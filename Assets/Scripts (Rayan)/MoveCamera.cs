b60ba352ee06ce646330db8f336e74437d8f03d4
Hatman-manhat <147822740+Hatman-manhat@users.noreply.github.com>
2024-11-19 10:20:11 +0100

    Interact changed

--- Assets/Scripts (Rayan)/MoveCamera.cs
+++ Assets/Scripts (Rayan)/MoveCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    public Transform cameraPosition;
+    [SerializeField] Transform cameraPosition;
 
     // Update is called once per frame
     void Update()
