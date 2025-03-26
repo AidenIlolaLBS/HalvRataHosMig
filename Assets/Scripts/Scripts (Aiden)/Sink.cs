36d09c9841dd116d357d5d4f8e05d0dc0a3597da
AidenIlolaLBS <aiden.ilola@elev.ga.lbs.se>
2025-03-26 18:33:38 +0100

    fixade saker
    fixade så handtaget är korrekt placerat så den kan roteras

--- Assets/Scripts/Scripts (Aiden)/Sink.cs
+++ Assets/Scripts/Scripts (Aiden)/Sink.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Sink : MonoBehaviour
 {
     public GameObject water;
+    public GameObject handle;
     private bool waterActive = false;
+    Quaternion targetRotaionZ = new();
+    public const float rotationSpeed = 1;
 
     private void Start()
     {
         water.SetActive(waterActive);
-    } 
+        targetRotaionZ = handle.transform.rotation;
+    }
+
+    private void Update()
+    {
+        handle.transform.rotation = Quaternion.Lerp(handle.transform.rotation, targetRotaionZ, Time.deltaTime * rotationSpeed);
+    }
 
     public void Interact()
     {
         if (waterActive)
         {
             waterActive = false;
+            targetRotaionZ = new Quaternion(handle.transform.rotation.x, handle.transform.rotation.y, -90,0);
         }
         else
         {
             waterActive = true;
+            targetRotaionZ = new Quaternion(handle.transform.rotation.x, handle.transform.rotation.y, 90, 0);
         }
         water.SetActive(waterActive);
     }
