af7747efe9a9891cc065ab406bb03585a0a73fcc
AidenIlola <ilolaaiden@gmail.com>
2025-03-27 08:29:46 +0100

    Försökte fixa skärbrädan
    behöve fixa så att den laddar in korrekt objekt. Sedan ta bort en tag

--- Assets/Scripts/Scripts (Aiden)/ChopBlock.cs
+++ Assets/Scripts/Scripts (Aiden)/ChopBlock.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ChopBlock : MonoBehaviour
 {
     public GameObject spawnLocation;
-    GameObject ingredient = null;
+    List<GameObject> choppedPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        choppedPrefabs = Resources.LoadAll<GameObject>("ChoppedPrefabs").ToList();
     }
 
     // Update is called once per frame
@@ -19,23 +20,27 @@ public class ChopBlock : MonoBehaviour
     }
     private void OnCollisionEnter(Collision other)
     {
-        if (ingredient == null)
+        if (other.gameObject.TryGetComponent(out InGameItemTags inGameItemTags))
         {
-            if (other.gameObject.TryGetComponent(out InGameItemTags inGameItemTags))
+            foreach (var item in inGameItemTags.Tags)
             {
-                foreach (var item in inGameItemTags.Tags)
+                if (item.TagName == "Choppable")
                 {
-                    if (item.TagName == "Choppable")
+                    other.transform.rotation = new Quaternion();
+                    float y = spawnLocation.transform.position.y + spawnLocation.gameObject.GetComponent<Renderer>().bounds.size.y/2 + other.gameObject.GetComponent<Renderer>().bounds.size.y / 2;
+                    Vector3 spawnVector = new(spawnLocation.transform.position.x, y, spawnLocation.transform.position.z);
+
+                    foreach (var choppedObject in choppedPrefabs)
                     {
-                        other.transform.rotation = new Quaternion();
-                        float y = spawnLocation.transform.position.y + spawnLocation.gameObject.GetComponent<Renderer>().bounds.size.y/2 + other.gameObject.GetComponent<Renderer>().bounds.size.y / 2;
-                        Vector3 spawnVector = new(spawnLocation.transform.position.x, y, spawnLocation.transform.position.z);
-                        other.transform.position = spawnVector;
-                        other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3();
-                        other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                        other.gameObject.tag = "Chop";
-                        ingredient = other.gameObject;
-                        return;
+                        GameObject temp = Instantiate(choppedObject, new Vector3(0, 5, 0), Quaternion.identity);
+                        temp.GetComponent<InspectorItemTags>().Start();
+                        foreach (var tagInfo in temp.GetComponent<InGameItemTags>().Tags)
+                        {
+                            if (tagInfo.tag == "")
+                            {
+
+                            }
+                        }
                     }
                 }
             }
