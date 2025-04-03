f82bb7ae775b4b96fc14246bcc865f0c27cdbb1d
AidenIlola <ilolaaiden@gmail.com>
2025-04-03 16:12:55 +0200

    fixade lite i matsal

---
+++ Assets/Scripts/Scripts (Aiden)/TestFocus.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestFocus : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
